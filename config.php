eab65ddaf7989e01565c5f2537d3e7a6557104ef
CyberTechnologyInc <cybertechnologyinc@msn.com>
2015-07-17 21:51:01 +0100

    Config file, used with the API.
    Contains settings for the API Key, DEV ID, and API URL.

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$config = new stdClass();
+$config->API_KEY = ""; //Get your API key from https://fs12.formsite.com/HiRez/form48/secure_index.html (if you get in you will be sent an email with the key)
+$config->DEV_ID = 0; //They will also send this to you in the email.
+$config->SITE_URL = "http://api.smitegame.com/smiteapi.svc/"; //Leave this be unless they change the API URL for some reason.
