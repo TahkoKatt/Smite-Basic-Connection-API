cba2800cc0f6aad64ab7050ab02ba58680834c77
CyberTechnologyInc <cybertechnologyinc@msn.com>
2015-07-30 19:14:20 +0100

    Easier to use and follow this example.

--- dataused.php
+++ dataused.php
@@ -1,14 +1,8 @@
 <?php
-session_start();
 error_reporting(E_ALL);
-require_once(getcwd()."/../login_system/API.php");
-require_once(getcwd()."/../login_system/config.php");
-include_once("API.php");
-include_once("config.php");
-$login = new LoginSystem($config);
-$API = new SmiteAPI($smiteConfig);
+require_once("autoloader.php");
 
-$data = $API->makeRequest("getdataused", "/DEV_ID/DEV_SIG/DEV_SES/TIMESTAMP", "JSON", true);
+$data = $SmiteAPI->makeRequest("getdataused", "/DEV_ID/DEV_SIG/DEV_SES/TIMESTAMP");
 $dailyRequestLimit = $data[0]->Request_Limit_Daily;
 $requestsSoFar = $data[0]->Total_Requests_Today;
 $totalRequestsLeft = $dailyRequestLimit-$requestsSoFar;
