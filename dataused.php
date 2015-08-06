0c03dfe33a40b89d810273301ac0c5c01384adbc
CyberTechnologyInc <cybertechnologyinc@msn.com>
2015-08-06 02:32:09 +0100

    Added a way to check how long the current session is active for
    Added a way to check how long the current session is active for

--- dataused.php
+++ dataused.php
@@ -3,15 +3,28 @@ error_reporting(E_ALL);
 require_once("autoloader.php");
 
 $data = $SmiteAPI->makeRequest("getdataused", "/DEV_ID/DEV_SIG/DEV_SES/TIMESTAMP");
-$dailyRequestLimit = $data[0]->Request_Limit_Daily;
-$requestsSoFar = $data[0]->Total_Requests_Today;
-$totalRequestsLeft = $dailyRequestLimit-$requestsSoFar;
+if(!empty($data[0])){
+	$q = $SmiteAPI->dbc->prepare("SELECT * FROM sessions WHERE expired = ?");
+	$q->bindValue(1, 0);
+	$q->execute();
 
-$activeSessions = $data[0]->Active_Sessions;
-$sessionCap = $data[0]->Session_Cap;
-$totalSessionsSoFar = $data[0]->Total_Sessions_Today;
-$totalSessionsLeft = $sessionCap-$totalSessionsSoFar;
+	$session_id = NULL;
+	$timestamp = NULL;
+	foreach($q->fetchAll() as $row){
+		$session_id = $row["session_id"];
+		$timestamp = $row["timestamp"];
+	}
 
-echo "Current Session ID: ".$API->getSessionID()."<br /><br />";
-echo "Requests used: ".$requestsSoFar."/".$dailyRequestLimit."<br />Total requests left: ".$totalRequestsLeft."<br /><br />";
-echo "Sessions used: ".$totalSessionsSoFar."/".$sessionCap."<br />Active sessions: ".$activeSessions."<br />Total sessions left: ".$totalSessionsLeft;
+	$dailyRequestLimit = $data[0]->Request_Limit_Daily;
+	$requestsSoFar = $data[0]->Total_Requests_Today;
+	$totalRequestsLeft = $dailyRequestLimit-$requestsSoFar;
+
+	$activeSessions = $data[0]->Active_Sessions;
+	$sessionCap = $data[0]->Session_Cap;
+	$totalSessionsSoFar = $data[0]->Total_Sessions_Today;
+	$totalSessionsLeft = $sessionCap-$totalSessionsSoFar;
+
+	echo "Requests used: ".$requestsSoFar."/".$dailyRequestLimit."<br />Total requests left: ".$totalRequestsLeft."<br /><br />";
+	echo "Sessions used: ".$totalSessionsSoFar."/".$sessionCap."<br />Active sessions: ".$activeSessions."<br />Total sessions left: ".$totalSessionsLeft."<br /><br />";
+	echo "Time left on active session: ".round((($timestamp-time())/60), 2)." minutes.";
+}
