868e4aab4829bd7b11a735c47cd4a0c583c48848
TahkoKatt <gbearson20@gmail.com>
2016-11-21 19:25:06 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 $smiteConfig = new stdClass();
-$smiteConfig->API_KEY = ""; //Get your API key from https://fs12.formsite.com/HiRez/form48/secure_index.html (if you get in you will be sent an email with the key)
-$smiteConfig->DEV_ID = 0; //They will also send this to you in the email.
+$smiteConfig->API_KEY = "BBB24395EE1749B9AE5EB94F1FC08615
+"; //Get your API key from https://fs12.formsite.com/HiRez/form48/secure_index.html (if you get in you will be sent an email with the key)
+$smiteConfig->DEV_ID = 1821; //They will also send this to you in the email.
 $smiteConfig->SITE_URL = "http://api.smitegame.com/smiteapi.svc/"; //Leave this be unless they change the API URL for some reason.
 $smiteConfig->SESSION_DURATION = 60*15; //15 minutes default (This is how long sessions last by default in Smite's API)
 
